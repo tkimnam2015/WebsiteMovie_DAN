adaaa7389854161177662004a642e3fda7d2147e
tkimnam2015 <tkimnam2015@gmail.com>
2022-10-09 14:11:40 +0700

    Create Hop phim xem sau

---
+++ WebsiteMovie_DAN/WebsiteMovie_DAN/Controllers/XemSauController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteMovie_DAN.Models;
+
+namespace WebsiteMovie_DAN.Controllers
+{
+    public class XemSauController : Controller
+    {
+        WebmovieDataContext data = new WebmovieDataContext();
+        // GET: XemSau
+        public ActionResult Index(string tendn)
+        {
+            var DSPhimBo = data.DSPhimBos.OrderByDescending(x => x.LuotXem).Take(3).ToList();
+            ViewData["TopPhim"] = DSPhimBo;
+            var tl = data.TheLoais.ToList();
+            var nam = data.Nams.ToList();
+            ViewData["TheLoai"] = tl;
+            ViewData["Nam"] = nam;
+            var quocgia = data.QuocGias.ToList();
+            ViewData["QuocGia"] = quocgia;
+            var hopphim = data.HopPhims.Where(m => m.TenDN.Equals(tendn)).ToList();
+            return View(hopphim);
+        }
+    }
+}
---
+++ WebsiteMovie_DAN/WebsiteMovie_DAN/Models/XemSau.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteMovie_DAN.Models
+{
+    public class XemSau
+    {
+        WebmovieDataContext data = new WebmovieDataContext();
+        public int iID { set; get; }
+        public string iTenPhim { set; get; }
+        public string hinh { set; get; }
+        public XemSau(int id)
+        {
+            iID = id;
+            DSPhimBo phim = data.DSPhimBos.Single(n => n.ID == iID);
+            iTenPhim = phim.TenPhim;
+            hinh = phim.Img;
+        }
+    }
+}
