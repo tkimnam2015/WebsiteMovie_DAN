82c35c5685a19f423a16fd26bcb43d60f84bf8fd
tkimnam2015 <tkimnam2015@gmail.com>
2022-10-09 15:41:51 +0700

    Update Them & Xoa xem sau

--- WebsiteMovie_DAN/WebsiteMovie_DAN/Controllers/XemSauController.cs
+++ WebsiteMovie_DAN/WebsiteMovie_DAN/Controllers/XemSauController.cs
@@ -24,5 +24,40 @@ namespace WebsiteMovie_DAN.Controllers
             var hopphim = data.HopPhims.Where(m => m.TenDN.Equals(tendn)).ToList();
             return View(hopphim);
         }
+
+        public ActionResult ThemVaoXemSau(string tendn, int idphim)
+        {
+            var quocgia = data.QuocGias.ToList();
+            var tl = data.TheLoais.ToList();
+            var nam = data.Nams.ToList();
+            ViewData["TheLoai"] = tl;
+            ViewData["Nam"] = nam;
+            ViewData["QuocGia"] = quocgia;
+
+            HopPhim phim = new HopPhim();
+            var dsphim = data.HopPhims.Where(m => m.TenDN == tendn).ToList();
+            foreach (var item in dsphim)
+            {
+                if (item.IDPhim == idphim)
+                    return RedirectToAction("ChiTietPhim", "ChiTietPhim", new { id = item.IDPhim });
+            }
+            phim.TenDN = tendn;
+            phim.IDPhim = idphim;
+            data.HopPhims.InsertOnSubmit(phim);
+            data.SubmitChanges();
+            return RedirectToAction("ChiTietPhim", "ChiTietPhim", new { id = idphim });
+        }
+        public ActionResult XoaXemSau(string tendn, int idphim)
+        {
+            HopPhim tl = data.HopPhims.SingleOrDefault(n => n.TenDN == tendn && n.IDPhim == idphim);
+            if (tl == null)
+            {
+                Response.SubStatusCode = 404;
+                return null;
+            }
+            data.HopPhims.DeleteOnSubmit(tl);
+            data.SubmitChanges();
+            return RedirectToAction("Index", new { tendn = tl.TenDN });
+        }
     }
 }
